1f946f95a190604ba579d40338da394f6504e653
josephrich98 <josephrich98@gmail.com>
2026-09-12 13:56:51 -0700

    iclr manuscript required false

--- paperpush/__init__.py
+++ paperpush/__init__.py
@@ -2,7 +2,7 @@
 
 import logging
 
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 __url__ = "https://github.com/pachterlab/paperpush"
 
 # Standard-library convention: a library should not configure logging output.
